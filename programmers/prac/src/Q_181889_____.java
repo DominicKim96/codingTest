10a6e79fdd3c38ee45fce9882df97f13df095406
DominicKim96 <kikidy96@gmail.com>
2024-01-18 18:50:16 +0900

    2024.01.18

---
+++ programmers/prac/src/Q_181889_____.java
@@ -0,0 +1,13 @@
+public class Q_181889_____ {
+    // 2024.01.18
+    /*
+    정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 n 번째 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요
+    */
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+}
